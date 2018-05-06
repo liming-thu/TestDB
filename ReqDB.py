77c5a3b472b2e62ab621ec591f6e87f271fe2085
Liming Dong <dlm14@mails.tsinghua.edu.cn>
2018-05-06 12:58:35 -0700

    test response time of different keywords of different popularity

--- ReqDB.py
+++ ReqDB.py
@@ -1,17 +1,64 @@
 import requests
 import json
-
 url='http://localhost:19002/query/service'
 headers = {"Content-Type": "application/x-www-form-urlencoded"}
-#payload="select VALUE coordinate from twitter.ds_tweet t where spatial_intersect(t.coordinate, create_rectangle(create_point(-109.060204,37.027935), create_point(-102.052130,41.003792))) and create_at>datetime(\"2017-08-08T00:00:00.000\") and create_at<datetime(\"2017-08-09T00:00:00.000\");"
-for i in range (1,2):
-    # payload="select VALUE coordinate from twitter.ds_tweet t where t.geo_tag.stateID="+str(i)+" and t.create_at>datetime(\"2017-08-08T00:00:00.000\") and t.create_at<datetime(\"2017-08-09T00:00:00.000\");"
-    payload="select VALUE coordinate from twitter.ds_tweet t where t.create_at>datetime(\"2017-08-08T00:00:00.000\") and t.create_at<datetime(\"2017-08-09T00:00:00.000\");"
-    r=requests.post(url,data=payload,headers=headers)
-    resArray=json.loads(r.text)["results"]
-    print i," len:",len(resArray)
-    if len(resArray)>0:
-        fo=open("us/"+str(i)+".dat",'w')
-        fo.write(str(resArray))
-        print i, "written"
-        fo.close()
+payload="select t.text from twitter.ds_tweet t where id=854093020022624256;"
+r=requests.post(url,data=payload,headers=headers)
+# resArray=json.loads(r.text)["results"]
+# f = open("db.dat",'w')
+# f.write(str(resArray))
+# f.close()
+# payload="select VALUE coordinate from twitter.ds_tweet t where spatial_intersect(t.coordinate, create_rectangle(create_point(-109.060204,37.027935), create_point(-102.052130,41.003792))) and create_at>datetime(\"2017-08-08T00:00:00.000\") and create_at<datetime(\"2017-08-09T00:00:00.000\");"
+# for str in ['liming',
+#             'China',
+#             'seven',
+#             'stone',
+#             'major',
+#             'games',
+#             'race',
+#             'peace',
+#             'table',
+#             'shirt',
+#             'action',
+#             'train',
+#             'success',
+#             'strong',
+#             'trump',
+#             'lunch',
+#             'university',
+#             'manager',
+#             'hospitality',
+#             'apply',
+#             'great',
+#             'work'
+#             ]:
+    # for i in range (1,11):
+    #     payload="select count(*) from twitter.ds_tweet t where ftcontains(t.text,['"+str+"']);"
+    #     r=requests.post(url,data=payload,headers=headers)
+    #     resArray=json.loads(r.text)["metrics"]
+    # print str,"select count time:",resArray['elapsedTime'][-2:]
+    # for i in range (1,11):
+    #      payload="select count(*) from twitter.ds_tweet t where ftcontains(t.text,['"+str+"']);"
+    #      r=requests.post(url,data=payload,headers=headers)
+    #      resArray=json.loads(r.text)["metrics"]
+    #      print resArray['elapsedTime'][0:8]
+    # ##################################################
+    # for i in range (1,11):
+    #     payload="select text from twitter.ds_tweet t where ftcontains(t.text,['"+str+"']);"
+    #     r=requests.post(url,data=payload,headers=headers)
+    #     resArray=json.loads(r.text)["metrics"]
+    #
+    # print str,"select text time:",resArray['elapsedTime'][-2:]
+    # for i in range (1,11):
+    # payload="select text from twitter.ds_tweet t where ftcontains(t.text,['"+str+"']);"
+    # r=requests.post(url,data=payload,headers=headers)
+    # resArray=json.loads(r.text)["metrics"]
+    # print resArray['elapsedTime'][0:8]
+        # print i," len:",len(resArray)
+        # if len(resArray)>0:
+        #     fo=open("us/"+str(i)+".json",'w')
+        #     fo.write(str(resArray))
+        #     # for coord in resArray:
+        #     #     fo.write(str(coord[0])+","+str(coord[1])+'\r')
+        #     print i, "written"
+        #     fo.close()
--- VAS.py
+++ VAS.py
@@ -1,52 +1,77 @@
 import math
 import numpy as np
 import json
-import threading
+import datetime
+
+
 # ary=[[1,1],[2,2],[3,3],[2.1,2.1],[3.2,3.2]]
 # print VAS(ary,4)
 
 def doVasFile(file):
     print file
-    f=open("state/"+file)
-    ln=f.readline().replace("None,","")
+    f = open(file)
+    ln = f.readline().replace("None,", "")
     f.close()
-    ary=np.asarray(json.loads(ln))
-    VAS(ary,len(ary)*0.01)
+    ary = np.asarray(json.loads(ln))
+    ######
+    print "doing vas", len(ary)
+    sampleList = VAS(ary, len(ary) * 0.01)
+    f = open("us/vas.json")
+    f.write(str(ary))
+    # for p in sampleList:
+    #     f.write(p[0] + "," + p[1] + '\r')
+    f.close()
+    print "write done."
     return
+
+
 def doVas(array):
-    VAS(array,len(array)*0.01)
-def VAS(PointArray,SampleSize):
-    SampleList=[]
+    VAS(array, len(array) * 0.01)
+
+
+def VAS(PointArray, SampleSize):
+    SampleList = []
+    i=0;
+    start=datetime.datetime.now()
     for point in PointArray:
-        if len(SampleList)<SampleSize:
-            Expand(SampleList,point)
+        i+=1
+        if i%10000==0:
+            print i,"time elapsed:",(datetime.datetime.now()-start).seconds,"s"
+        if len(SampleList) < SampleSize:
+            Expand(SampleList, point)
         else:
-            Expand(SampleList,point)
+            Expand(SampleList, point)
             Shrink(SampleList)
     return SampleList
 
-def Expand(SampleList,Point):
-    rsp=0
+
+def Expand(SampleList, Point):
+    rsp = 0
     for p in SampleList:
-        dist=Kappa(Point,p)
-        p[2]+=dist
-        rsp+=dist
-    SampleList.append([Point[0],Point[1],rsp])
+        dist = Kappa(Point, p)
+        p[2] += dist
+        rsp += dist
+    SampleList.append([Point[0], Point[1], rsp])
     return
+
+
 def Shrink(SampleList):
-    maxRsp=0
-    index=0
-    for i in range(0,len(SampleList)):
-        if SampleList[i][2]>maxRsp:
-            maxRsp=SampleList[i][2]
-            index=i
-    for i in range(0,len(SampleList)):
-        if i!=index:
-            SampleList[i][2]-=Kappa(SampleList[index],SampleList[i])
+    maxRsp = 0
+    index = 0
+    for i in range(0, len(SampleList)):
+        if SampleList[i][2] > maxRsp:
+            maxRsp = SampleList[i][2]
+            index = i
+    for i in range(0, len(SampleList)):
+        if i != index:
+            SampleList[i][2] -= Kappa(SampleList[index], SampleList[i])
     del SampleList[index]
     return
 
-def Kappa(x,y):
-    distSquare=(x[0]-y[0])*(x[0]-y[0])+(x[1]-y[1])*(x[1]-y[1])
+
+def Kappa(x, y):
+    distSquare = (x[0] - y[0]) * (x[0] - y[0]) + (x[1] - y[1]) * (x[1] - y[1])
     return math.exp(-distSquare)
-# doVasFile("1.dat")
+
+
+doVasFile("us/10M.json")
